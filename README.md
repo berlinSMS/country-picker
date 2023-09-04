e2898e47e9cf57e6c81c961792804c1118e2f640
haksi <meutzner@berlinsms.de>
2023-09-04 21:29:26 +0200

    name changes

---
+++ README.md
@@ -0,0 +1,60 @@
+# jQuery CountryPicker
+[Homepage](https://www.berlinsms.de/)
+
+# Usage
+
+Include jquery
+
+    
+```html
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+```
+
+Download bsms-country-picker.js and 
+```link
+https://raw.githubusercontent.com/berlinSMS/country-picker/main/bsms-code-digits.js
+https://raw.githubusercontent.com/berlinSMS/country-picker/main/all-countries.js
+```
+
+Include bsms-country-picker and all-countries.js
+```html
+<script src="bsms-country-picker.js"></script>
+<script src="all-countries.js"></script>
+```
+
+Find your jquery-container and assign country-picker
+```js
+$('.plugin-container').bsmsCountryPicker();    
+```
+
+Make sure, the script is fully loaded, before you assign country-picker, e.g. use jquerys 'ready'
+```js
+$(document).ready(function () {
+    $('.plugin-container').bsmsCountryPicker();
+});    
+```
+
+Add options, if needed:
+```js
+$(document).ready(function() {
+	$('.country-picker-container').bsmsCountryPicker({
+		countries: { 
+			"AT": { name:"Österreich",  code:"+43"} ,
+			"DE": { name:"Deutschland", code:"+49"}, 
+			"CH": { name:"Schweiz",     code:"+41"}, 
+		}, 
+		defaultCountry: "DE"
+	});
+});   
+```
+
+# Options
+
+
+| Option         | DESCRIPTION                                                                                                                                     | DEFAULT                               |
+|----------------|-------------------------------------------------------------------------------------------------------------------------------------------------|---------------------------------------|
+| countries      | Dictionary of available countries, each key must be an uniqued identifier of a country, values are dictionaries of name and code of the country | {"DE": { name:"Germany", code:"+49"}} |
+| defaultCountry | identifier of a country, which ist selected initialy, has to be a key in the countries-dictionary                                               | DE                                    |
+| inputName      | name-attribute of a hidden input-tag, which the plugin includes to the form                                                                     | bsms-country-code                     |
+| className      | for costum-css, class of the dropdown-container                                                                                                 | bsms-country-code                     |         
+                                                                                                                                                                           
--- bsms-country-picker.js
+++ bsms-country-picker.js
@@ -1,6 +1,51 @@
-// countryDropdown.js
-(function($) {
-    $.fn.countryDropdown = function(options) {
+/** 
+ * A jquery-plugin to pick a country-code in a formular
+ *  - You need to import jquery
+ *
+ * @copyright Copyright (c) 2023, Energieweise Ingenieur GmbH, berlinsms
+ * @link      https://www.berlinsms.de/
+ * 
+ * Hiermit wird jeder Person, die eine Kopie dieser Software und der zugehoerigen
+ * Dokumentationsdateien (die "Software") erwirbt, kostenlos die Erlaubnis erteilt, 
+ * uneingeschraenkt mit der Software zu handeln, einschliesslich und ohne 
+ * Einschraenkung der Rechte, Kopien der Software zu verwenden, zu kopieren, zu 
+ * modifizieren, zusammenzufuehren, zu veroeffentlichen, zu vertreiben, zu 
+ * unterlizenzieren und/oder zu verkaufen, und Personen, denen die Software zur 
+ * Verfuegung gestellt wird, dies unter den folgenden Bedingungen zu gestatten:
+ * 
+ * Dieser Copyright-Hinweis und dieser Genehmigungshinweis muessen in allen Kopien 
+ * oder wesentlichen Teilen der Software enthalten sein.
+ * 
+ * DIE SOFTWARE WIRD OHNE MAENGELGEWAEHR ZUR VERFUEGUNG GESTELLT, OHNE AUSDRUECKLICHE 
+ * ODER STILLSCHWEIGENDE GEWAEHRLEISTUNG JEGLICHER ART, EINSCHLIESSLICH, ABER NICHT
+ * BESCHRAENKT AUF DIE GEWAEHRLEISTUNG DER MARKTGAENGIGKEIT, DER EIGNUNG FUER EINEN 
+ * BESTIMMTEN ZWECK UND DER NICHTVERLETZUNG VON RECHTEN. IN KEINEM FALL HAFTEN DIE 
+ * AUTOREN ODER URHEBERRECHTSINHABER FUER JEGLICHE ANSPRUECHE, SCHAEDEN ODER SONSTIGE 
+ * HAFTUNG, SEI ES DURCH VERTRAG, UNERLAUBTE HANDLUNGEN ODER ANDERWEITIG, DIE SICH 
+ * AUS DER SOFTWARE ODER DER NUTZUNG DER SOFTWARE ODER DEM SONSTIGEN UMGANG MIT 
+ * DER SOFTWARE ERGEBEN ODER DAMIT ZUSAMMENHAENGEN.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in 
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+ * Software, and to permit persons to whom the Software is furnished to do so, 
+ * subject to the following conditions:
+ * 
+ * This copyright notice and this permission notice must be included in all copies 
+ * or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR
+ * IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIMS, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN CONTRACT, TORT OR OTHERWISE, ARISING OUT OF OR RELATED TO THE SOFTWARE 
+ * OR THE USE OF OR OTHER DEALINGS WITH THE SOFTWARE.
+ * 
+ */
+ 
+ (function($) {
+    $.fn.bsmsCountryPicker = function(options) {
         
 		if (!options.className) options.className = 'bsms-country-code';
 		
